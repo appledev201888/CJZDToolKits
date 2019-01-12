89b8ad8b0b9a5e43e8d7966ebcfe6566f6bd9c4a
chenweibin <weibinchen@uutaka.com>
2019-01-12 21:37:58 +0800

    提交的描述说明

---
+++ Classes/CJZToolKits.framework/Headers/CJZToolKits.h
@@ -0,0 +1,14 @@
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CJZToolKits.
+FOUNDATION_EXPORT double CJZToolKitsVersionNumber;
+
+//! Project version string for CJZToolKits.
+FOUNDATION_EXPORT const unsigned char CJZToolKitsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CJZToolKits/PublicHeader.h>
+
+#import <CJZToolKits/CJZToolKitsManage.h>
+
+
---
+++ Classes/CJZToolKits.framework/Headers/CJZToolKitsManage.h
@@ -0,0 +1,13 @@
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface CJZToolKitsManage : NSObject
+
++ (void)CJZToolKitsManage:(void(^)(NSString *temp))block;
+
+@end
+
+NS_ASSUME_NONNULL_END
